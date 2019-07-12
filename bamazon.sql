9ae57e3d67b0f707a6942927c7ed6811e1da91fe
Sebastian Hernandez <40608603+sbstiane46@users.noreply.github.com>
2019-07-12 16:45:14 -0400

    Add files via upload

---
+++ bamazon.sql
@@ -0,0 +1,8 @@
+CREATE TABLE products (
+    item_id VARCHAR(100)
+    product_name VARCHAR (100) NULL,
+    department_name VARCHAR (100) NULL,
+    price DECIMAL (100,2) NULL,
+    stock_quantity INT NULL,
+
+)
