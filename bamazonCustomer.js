477cf4c03183fd250a1f93fdf7a9c6c806bdf92c
sbstiane46 <sbstian89@yahoo.com>
2019-07-16 17:52:54 -0400

    progress

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-    host:"my local",
+    host:"localhost",
     port:3306,
     user:"root",
     password:"123abc",
@@ -12,4 +12,14 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     console.log("connection Successful");
+    makeTable();
 })
+
+var makeTable = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        for(var i=0; i<res.length; i++){
+            console.log(res[i].item_id+" || "+res[i].product_name+" || "+
+            res[i].department_name+" || "+res[i].price+" || "+res[i].stock_quantity+"\n");
+        }
+    })
+}
