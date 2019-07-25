888cdabc273eb82f354cae63bc101151f1122efb
sbstiane46 <sbstian89@yahoo.com>
2019-07-25 16:53:18 -0400

    update

--- bamazon.sql
+++ bamazon.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
 CREATE TABLE products(
@@ -8,3 +9,16 @@ CREATE TABLE products(
     stock_quantity INT (12) not NULL,
     primary key (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+("guitar", "instruments", 599.99, 30),
+("exhaust", "automotive", 2999.99, 16),
+("harmonica", "instruments", 199.99, 40),
+("suspension kit", "automotive", 1800.99, 8),
+("table alarm", "home goods", 19.99, 17),
+("lamp", "home goods", 199.99, 15),
+("toyota headlights", "automotive", 499.99, 25)
+
+SELECT * FROM products;
+
