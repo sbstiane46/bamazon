605a8425bd283e1923c1ae54fc27f7fe3803d918
sbstiane46 <sbstian89@yahoo.com>
2019-09-16 20:33:46 -0400

    added images

--- bamazon.sql
+++ bamazon.sql
@@ -5,8 +5,8 @@ CREATE TABLE products(
     item_id integer auto_increment not null,
     product_name VARCHAR (50) not NULL,
     department_name VARCHAR (50) not NULL,
-    price DECIMAL (10,4) not NULL,
-    stock_quantity INT (12) not NULL,
+    price DECIMAL (10,2) not NULL,
+    stock_quantity INT NOT NULL,
     primary key (item_id)
 );
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,9 +19,82 @@ connection.connect(function(err){
 
 var makeTable = function(){
     connection.query("SELECT * FROM products", function(err,res){
+        if (err) throw err;
+        
         for(var i=0; i<res.length; i++){
-            console.log(res[i].item_id+" || "+res[i].product_name+" || "+
-            res[i].department_name+" || "+res[i].price+" || "+res[i].stock_quantity+"\n");
+            console.log("ID: " +res[i].item_id+ " ||Product Name: "+res[i].product_name+ " ||Department: " +res[i].department_name+
+            " ||Price: "+res[i].price+" ||Quantity: "+res[i].stock_quantity+"\n");
+            console.log("==================================================");
         }
+        promptCustomer(res);
     })
 }
+
+var promptCustomer = function(res) {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "id",
+        message: "Select item's ID to purchase.\n",
+        validate: function(value) {
+          if (!isNaN(value) && value < 31) {
+            return true;
+          }
+          return false;
+        }
+       },
+   
+       {
+        type: "input",
+        name: "quantity",
+        message: "Enter quantity. \n",
+        validate: function(value) {
+          if (!isNaN(value)) {
+            return true;
+          }
+          return false;
+         }
+       }
+    ]).then(function(answer){
+        var userId = answer.id;
+                console.log("Chosen item id: " , userId);
+    
+                var userQuantity = answer.quantity;
+                console.log("Chosen quantity from stock: " , userQuantity , "\n");
+    
+                connection.query("SELECT * FROM products WHERE ?", [{ item_id : answer.id }], function(err, res) {
+                    if (err) throw err;
+                    
+                    
+                    console.table(res);
+                    var stock_quantity = res[0].stock;
+                    console.log("# in stock: " , stock_quantity);
+                    var price = res[0].price;
+                    var remaining_quantity = stock_quantity - answer.quantity;
+                    console.log("Remaining: " , remaining_quantity);
+    
+                    if(stock_quantity > answer.quantity) {
+    
+                        console.log("Remaining: " + remaining_quantity);
+    
+                        connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?",
+                        [
+                        remaining_quantity, answer.id
+                        ],
+                        );
+
+                        connection.query("SELECT * FROM products", function(err, res) {
+    
+                            console.log("Updated Inventory: ");
+                            console.table(res);
+                        });
+    
+                    } else {
+                        console.log("Not enough available!");
+                    }
+    
+                connection.end();
+    
+                });
+            })
+      }    
